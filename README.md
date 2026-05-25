33d7114e4358b573c98fb66c8717d072a4833943
NetRunner <128007180+net-learner-04@users.noreply.github.com>
2026-05-25 04:59:39 +0000

    section 9 task commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # python-study
 Learning Python through Udemy
+
+
+git add . && git commit -m "section 9 task commit" && git push
---
+++ section-9-task.py
@@ -0,0 +1,39 @@
+logo = r'''               
+   ___________
+   \         /
+    )_______(
+    |"""""""|_.-._,.---------.,_.-._
+    |       | | |               | | ''-.
+    |       |_| |_             _| |_..-'
+    |_______| '-' `'---------'` '-'
+    )"""""""(
+   /_________\
+   `'-------'`
+  .-------------.
+ /_______________\
+'''
+
+bidder_info = {}
+end_of_auction = False
+max_value = 0
+winner = ""
+
+while not end_of_auction:
+    print(logo)
+    print("\nWelcome to the secret auction program.")
+    name = input("What is your name?: ")
+    bid = int(input("What's your bid?: $"))
+    
+    bidder_info[name] = bid
+    
+    if input("Are there any other bidders? Type 'yes' or 'no'.\n").lower() == "no":
+        over = True
+    else:
+        print("\n" * 100)
+
+for bidder in bidder_info:
+    if bidder_info[bidder] > max_value:
+        max_value = bidder_info[bidder]
+        winner = bidder
+        
+print(f"The winner is {winner} with a bid of ${max_value}.")
